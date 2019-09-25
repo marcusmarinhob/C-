5bc51fceb91b7e2289c5eaaca907a6375258166c
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-25 20:46:57 -0300

    1. Created first example with strings

---
+++ Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string consoleMsg = "Marcus Marinho - C# Study";
+            Console.WriteLine(consoleMsg);
+        }
+    }
+}
