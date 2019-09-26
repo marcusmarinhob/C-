96b44db15a52f2a2e7710a8a8e26d91807ca1cf6
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-26 20:17:48 -0300

    1. Using add operator

--- Course/Operators/Program.cs
+++ Course/Operators/Program.cs
@@ -6,7 +6,14 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Marcus";
+            double salary_1 = 100.0;
+            double salary_2 = 250.0;
+            double soma = salary_1 + salary_2;
+
+            string consoleMessage = name + " receives R$" + soma + " in a month!";
+                        
+            Console.WriteLine(consoleMessage);
         }
     }
 }
