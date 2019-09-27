be8b12c1f2e1db3fe9979d5f31094940c86a5ff3
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-26 22:01:19 -0300

    1. Reading data from console.

---
+++ Course/InputFromUser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InputFromUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.Write("Digite o nome: ");
+            nome = Console.ReadLine();  
+
+            Console.WriteLine("O nome digitado foi: " + nome);
+        }
+    }
+}
